99a37f6810793b385d5e02f474c4b407107bd67b
orisjineman <park940529@gmail.com>
2026-08-29 18:39:20 +0900

    init

---
+++ src/test/java/com/github/orisjineman/hexagonalpractice/HexagonalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.orisjineman.hexagonalpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
