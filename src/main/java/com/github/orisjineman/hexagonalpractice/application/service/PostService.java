8a32689ae591aa47c539019a29ac860465bf0a09
orisjineman <park940529@gmail.com>
2026-08-29 19:01:54 +0900

    feat: service

---
+++ src/main/java/com/github/orisjineman/hexagonalpractice/application/service/PostService.java
@@ -0,0 +1,20 @@
+package com.github.orisjineman.hexagonalpractice.application.service;
+
+import com.github.orisjineman.hexagonalpractice.application.port.in.CreatePostUseCase;
+import com.github.orisjineman.hexagonalpractice.application.port.out.PostRepository;
+import com.github.orisjineman.hexagonalpractice.domain.Post;
+
+public class PostService implements CreatePostUseCase {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public Post createPost(String title, String content) {
+        Post post = Post.create(title, content);
+        return postRepository.save(post);
+    }
+}
